8873d7db7af2f05c47cfb3a5de0aa56984b97c24
brendanneufeld <brendan@jam3.com>
2015-11-02 10:18:46 -0500

    started dom-pareser

--- example-simple-button/index.js
+++ example-simple-button/index.js
@@ -7,47 +7,23 @@ var f1SimpleButton = require('../lib/f1-simple-button');
 var container = document.body;
 container.style.backgroundColor = "#ccc";
 
-var slotPosRightDirLeft = f1SimpleButton();
+var slotPosRightDirLeft = f1SimpleButton({
+    offBgBackgroundColor: [46, 204, 114],
+    offBorderBorderColor: [256, 256, 256],
+    offIconPosition: [324, 27, 0],
+    offIconColor: [256, 256, 256],
+    offTextColor: [256, 256, 256],
+
+    overBgBackgroundColor: [36, 182, 98],
+    overIconPosition: [188, 27, 0],
+
+    downBorderBorderColor: [23, 149, 76],
+    downIconColor: [23, 149, 76],
+    downTextColor: [23, 149, 76]
+});
+
 container.appendChild(slotPosRightDirLeft.bg);
 
 
-
-function getBtnTextPosition(txt){
-    return [txt.offsetLeft, txt.offsetTop, 0]
-}
-
-function getIconOverPosition(targs, opts){
-
-    if(!targs)  console.warn('You need to supply "getIconOverPosition()", a target object.');
-
-    opts = opts || {
-            position: 'right',
-            direction: 'left'
-        };
-
-    var edgeOfText = targs.text.offsetLeft + targs.text.offsetWidth;
-    var availSpace = targs.bg.offsetWidth - edgeOfText - borderWidth;
-
-    var left = edgeOfText + ((availSpace - targs.icon.offsetWidth) * .5);
-    var top = (targs.bg.offsetHeight - targs.icon.offsetHeight) * .5;
-
-    return [Math.floor(left), Math.floor(top), 0];
-}
-
-function getIconOffPosition(targs, opts){
-
-    if(!targs)  console.warn('You need to supply "getIconOverPosition()", a target object.');
-
-    opts = opts || {
-            position: 'right',
-            direction: 'left'
-        };
-
-    var left = targs.bg.offsetWidth * 1.3;
-    var top = (targs.bg.offsetHeight - targs.icon.offsetHeight) * .5;
-
-    return [left, top, 0];
-
-}
 
 
