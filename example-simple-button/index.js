958a308c971beea5a8149993ac0edbd8facbad16
brendanneufeld <brendan@jam3.com>
2015-11-05 16:50:18 -0500

    override bug fixed

--- example-simple-button/index.js
+++ example-simple-button/index.js
@@ -7,14 +7,32 @@ var f1SimpleButton = require('../lib/f1-simple-button');
 var container = document.body;
 container.style.backgroundColor = "#ccc";
 
-var slotPosRightDirLeft = f1SimpleButton({
-    offBgBackgroundColor: [46, 204, 114],
+var simpleDefaultBtn = f1SimpleButton();
+
+var simpleBtn = f1SimpleButton({
+    offBackgroundBackgroundColor: [46, 204, 114],
+    offBorderBorderColor: [256, 256, 256],
+    offIconPosition: [188, 27, 0],
+    offIconColor: [256, 256, 256],
+    offTextColor: [256, 256, 256],
+
+    overBackgroundBackgroundColor: [36, 182, 98],
+    //overIconPosition: [188, 27, 0],
+
+    downBorderBorderColor: [23, 149, 76],
+    downIconColor: [23, 149, 76],
+    downTextColor: [23, 149, 76]
+});
+
+var slotPosRightDirLeftBtn = f1SimpleButton({
+    slotMachine: true,
+    offBackgroundBackgroundColor: [46, 204, 114],
     offBorderBorderColor: [256, 256, 256],
     offIconPosition: [324, 27, 0],
     offIconColor: [256, 256, 256],
     offTextColor: [256, 256, 256],
 
-    overBgBackgroundColor: [36, 182, 98],
+    overBackgroundBackgroundColor: [36, 182, 98],
     overIconPosition: [188, 27, 0],
 
     downBorderBorderColor: [23, 149, 76],
@@ -22,7 +40,9 @@ var slotPosRightDirLeft = f1SimpleButton({
     downTextColor: [23, 149, 76]
 });
 
-container.appendChild(slotPosRightDirLeft.bg);
+container.appendChild(simpleDefaultBtn.background);
+container.appendChild(simpleBtn.background);
+container.appendChild(slotPosRightDirLeftBtn.background);
 
 
 
